f9f30f2f55db8e7ffc803fe48635aa27cf28f232
CorneliakKelinske <corneliakelinske@gmail.com>
2025-11-17 14:53:33 -0800

    update git ignore

--- assets/phoenix_live_view/js/phoenix_live_view/dom_post_morph_restorer.js
+++ assets/phoenix_live_view/js/phoenix_live_view/dom_post_morph_restorer.js
@@ -1,35 +1,40 @@
-import {
-  maybe
-} from "./utils"
+import { maybe } from "./utils";
 
-import DOM from "./dom"
+import DOM from "./dom";
 
 export default class DOMPostMorphRestorer {
   // lvp:comment-next-line
-  // constructor(containerBefore, containerAfter, updateType){
+  // constructor(containerBefore, containerAfter, updateType) {
   // lvp:add-next-line
-  constructor(containerBefore, containerAfter, updateType, domRoot){
-    let idsBefore = new Set()
-    let idsAfter = new Set([...containerAfter.children].map(child => child.id))
+  constructor(containerBefore, containerAfter, updateType, domRoot) {
+    const idsBefore = new Set();
+    const idsAfter = new Set(
+      [...containerAfter.children].map((child) => child.id),
+    );
 
-    let elementsToModify = []
+    const elementsToModify = [];
 
-    Array.from(containerBefore.children).forEach(child => {
-      if(child.id){ // all of our children should be elements with ids
-        idsBefore.add(child.id)
-        if(idsAfter.has(child.id)){
-          let previousElementId = child.previousElementSibling && child.previousElementSibling.id
-          elementsToModify.push({elementId: child.id, previousElementId: previousElementId})
+    Array.from(containerBefore.children).forEach((child) => {
+      if (child.id) {
+        // all of our children should be elements with ids
+        idsBefore.add(child.id);
+        if (idsAfter.has(child.id)) {
+          const previousElementId =
+            child.previousElementSibling && child.previousElementSibling.id;
+          elementsToModify.push({
+            elementId: child.id,
+            previousElementId: previousElementId,
+          });
         }
       }
-    })
+    });
 
+    this.containerId = containerAfter.id;
+    this.updateType = updateType;
+    this.elementsToModify = elementsToModify;
+    this.elementIdsToAdd = [...idsAfter].filter((id) => !idsBefore.has(id));
     // lvp:add-next-line
-    this.domRoot = domRoot || document
-    this.containerId = containerAfter.id
-    this.updateType = updateType
-    this.elementsToModify = elementsToModify
-    this.elementIdsToAdd = [...idsAfter].filter(id => !idsBefore.has(id))
+    this.domRoot = domRoot || document;
   }
 
   // We do the following to optimize append/prepend operations:
@@ -38,48 +43,63 @@ export default class DOMPostMorphRestorer {
   //      by storing the id of their previous sibling
   //   3) New elements are going to be put in the right place by morphdom during append.
   //      For prepend, we move them to the first position in the container
-  perform(){
+  perform() {
     // lvp:comment-next-line
-    // let container = DOM.byId(this.containerId)
+    // const container = DOM.byId(this.containerId);
     // lvp:add-next-line
-    let container = DOM.byId(this.containerId, this.domRoot)
-    this.elementsToModify.forEach(elementToModify => {
-      if(elementToModify.previousElementId){
-      // lvp:comment-start
-      // maybe(document.getElementById(elementToModify.previousElementId), previousElem => {
-      //   maybe(document.getElementById(elementToModify.elementId), elem => {
-      // lvp:comment-end
-      // lvp:block-start
-        maybe(this.domRoot.getElementById(elementToModify.previousElementId), previousElem => {
-          maybe(this.domRoot.getElementById(elementToModify.elementId), elem => {
-            // lvp:block-end
-            let isInRightPlace = elem.previousElementSibling && elem.previousElementSibling.id == previousElem.id
-            if(!isInRightPlace){
-              previousElem.insertAdjacentElement("afterend", elem)
-            }
-          })
-        })
+    const container = DOM.byId(this.containerId, this.domRoot);
+    if (!container) {
+      return;
+    }
+    this.elementsToModify.forEach((elementToModify) => {
+      if (elementToModify.previousElementId) {
+        // lvp:comment-next-line
+        // maybe(
+        //   document.getElementById(elementToModify.previousElementId),
+        // lvp:add-next-line
+        maybe(
+          this.domRoot.getElementById(elementToModify.previousElementId),
+          (previousElem) => {
+            // lvp:comment-next-line
+            // maybe(
+            //   document.getElementById(elementToModify.elementId),
+            // lvp:add-next-line
+            maybe(
+              this.domRoot.getElementById(elementToModify.elementId),
+              (elem) => {
+                const isInRightPlace =
+                  elem.previousElementSibling &&
+                  elem.previousElementSibling.id == previousElem.id;
+                if (!isInRightPlace) {
+                  previousElem.insertAdjacentElement("afterend", elem);
+                }
+              },
+            );
+          },
+        );
       } else {
         // This is the first element in the container
         // lvp:comment-next-line
-        // maybe(document.getElementById(elementToModify.elementId), elem => {
+        // maybe(document.getElementById(elementToModify.elementId), (elem) => {
         // lvp:add-next-line
-        maybe(this.domRoot.getElementById(elementToModify.elementId), elem => {
-          let isInRightPlace = elem.previousElementSibling == null
-          if(!isInRightPlace){
-            container.insertAdjacentElement("afterbegin", elem)
+        maybe(this.domRoot.getElementById(elementToModify.elementId), (elem) => {
+          const isInRightPlace = elem.previousElementSibling == null;
+          if (!isInRightPlace) {
+            container.insertAdjacentElement("afterbegin", elem);
           }
-        })
+        });
       }
-    })
+    });
 
-    if(this.updateType == "prepend"){
-      this.elementIdsToAdd.reverse().forEach(elemId => {
+    if (this.updateType == "prepend") {
+      this.elementIdsToAdd.reverse().forEach((elemId) => {
         // lvp:comment-next-line
-        // maybe(document.getElementById(elemId), elem => container.insertAdjacentElement("afterbegin", elem))
+        // maybe(document.getElementById(elemId), (elem) =>
         // lvp:add-next-line
-        maybe(this.domRoot.getElementById(elemId), elem => container.insertAdjacentElement("afterbegin", elem))
-      })
+        maybe(this.domRoot.getElementById(elemId), (elem) =>
+          container.insertAdjacentElement("afterbegin", elem),
+        );
+      });
     }
   }
 }
